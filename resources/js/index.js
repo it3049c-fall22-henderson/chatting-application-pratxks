429e8eb3891c888800d8ed1f23de61fe1d541d29
pratxks <101081557+pratxks@users.noreply.github.com>
2022-11-28 19:58:50 +0530

    Initialised index.js as per instructions

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,4 +2,97 @@ const nameInput = document.getElementById("my-name-input");
 const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
+const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
 
+
+function updateMessages() {
+  // Fetch Messages
+  // Loop over the messages. Inside the loop we will
+      // get each message
+      // format it
+      // add it to the chatbox
+}
+
+
+function fetchMessages() {
+    return fetch(serverURL)
+        .then( response => response.json())
+}
+
+async function updateMessages() {
+  // Fetch Messages
+  const messages = await fetchMessages();
+
+  // Loop over the messages. Inside the loop we will
+      // get each message
+      // format it
+      // add it to the chatbox
+}
+
+function sendMessages(username, text) {
+  const newMessage = {
+      sender: username,
+      text: text,
+      timestamp: new Date()
+  }
+
+  fetch (serverURL, {
+      method: `POST`, 
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newMessage)
+  });
+}
+
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+
+  if (myNameInput === message.sender) {
+      return `
+      <div class="mine messages">
+          <div class="message">
+              ${message.text}
+          </div>
+          <div class="sender-info">
+              ${formattedTime}
+          </div>
+      </div>
+      `
+  } else {
+      return `
+          <div class="yours messages">
+              <div class="message">
+                  ${message.text}
+              </div>
+              <div class="sender-info">
+                  ${message.sender} ${formattedTime}
+              </div>
+          </div>
+      `
+  }
+}
+
+async function updateMessages() {
+  // Fetch Messages
+  const messages = await fetchMessages();
+  // Loop over the messages. Inside the loop we will:
+      // get each message
+      // format it
+      // add it to the chatbox
+  let formattedMessages = "";
+  messages.forEach(message => {
+      formattedMessages += formatMessage(message, nameInput.value);
+  });
+  chatBox.innerHTML = formattedMessages;
+}
+
+sendButton.addEventListener("click", function(sendButtonClickEvent) {
+  sendButtonClickEvent.preventDefault();
+  const sender = nameInput.value;
+  const message = myMessage.value;
+
+  sendMessages(sender,message);
+  myMessage.value = "";
+});
